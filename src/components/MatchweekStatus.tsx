6f1bbb569b2f9c4eb1a0c44b0c25a815060742b7
Sohrab Chegini <sohrab.chegini@gmail.com>
2022-08-21 12:38:50 +0430

    First draft of matchweek status component

---
+++ src/components/MatchweekStatus.tsx
@@ -0,0 +1,12 @@
+import "../styles/MatchweekStatus.css"
+
+const MatchweekStatus = () => {
+  return (
+    <div className="matchweek-status">
+      <span id="matchweek-date">شنبه ۳۰ مرداد ۱۴۰۰ - ساعت ۱۷</span>
+      <span id="matchweek-number">هفته سوم</span>
+    </div>
+  )
+}
+
+export default MatchweekStatus
