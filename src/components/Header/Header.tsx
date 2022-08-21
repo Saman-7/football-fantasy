19ee518d51401af0ca740690e85271010940246a
Sohrab Chegini <sohrab.chegini@gmail.com>
2022-08-21 16:02:34 +0430

    Create header folder

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import PLHeader from "../../images/Header.svg"
+
+const Header = () => {
+  return (
+    <img
+      src={PLHeader}
+      style={{
+        width: "100%",
+      }}
+      alt="Premier league header"
+    />
+  )
+}
+
+export default Header
