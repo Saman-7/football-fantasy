51dde59ad6cd3c92b336f1f95100d994d0e064d0
Sohrab Chegini <sohrab.chegini@gmail.com>
2022-08-31 14:54:30 +0430

    Refactor remaining players component

---
+++ src/components/RemainingPlayers/RemainingPlayers.styled.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+
+export const RemainingPlayersContainer = styled.div`
+  width: 17.0625rem;
+  height: 8.0625rem;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background: linear-gradient(93.79deg, #04f7da -6.31%, #02fda2 118.26%);
+  box-shadow: 0rem 0rem 0.625rem rgba(0, 0, 0, 0.15);
+  border-radius: 1rem 1rem 0rem 0rem;
+
+  & > span {
+    font-weight: 900;
+    font-size: 36px;
+    color: #3d195b;
+  }
+`
+
+export const RemainingPlayersRightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & img {
+    width: 2.2713rem;
+    height: 2.5rem;
+  }
+
+  & span {
+    margin-top: 0.3125rem;
+  }
+`
--- src/components/RemainingPlayers/RemainingPlayers.tsx
+++ src/components/RemainingPlayers/RemainingPlayers.tsx
@@ -1,71 +1,21 @@
-import styled from "styled-components";
-import Logo from "../../svg/Vector.svg";
-
-const RemainingPlayersContainer = styled.div`
-  width: 273px;
-  height: 129px;
-  background: linear-gradient(93.79deg, #04f7da -6.31%, #02fda2 118.26%);
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
-  border-radius: 16px 16px 0px 0px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const RemainingPlayersFractionContainer = styled.div`
-  position: relative;
-  left: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const RemainingPlayersFraction = styled.span`
-  font-family: "Vazirmatn";
-  font-style: normal;
-  font-weight: 900;
-  font-size: 36px;
-  color: #3d195b;
-`;
-
-const RemainingPlayersLogoContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  position: relative;
-  right: 18px;
-`;
-
-const RemainingPlayersLogo = styled.img`
-  width: 36.34px;
-  height: 40px;
-  position: relative;
-`;
-
-const RemainingPlayersLogoContainerText = styled.span`
-  font-family: "Vazirmatn";
-  font-style: normal;
-  font-weight: 900;
-  font-size: 14px;
-  color: #3d195b;
-`;
+import {
+  RemainingPlayersContainer,
+  RemainingPlayersRightContainer,
+} from "./RemainingPlayers.styled"
+import RemainingPlayersSVG from "../../svg/remaining-players.svg"
 
 const RemainingPlayers = () => {
   return (
     <RemainingPlayersContainer>
-      <RemainingPlayersFractionContainer>
-        <RemainingPlayersFraction>۱۵/۱۲</RemainingPlayersFraction>
-      </RemainingPlayersFractionContainer>
+      <RemainingPlayersRightContainer>
+        <img src={RemainingPlayersSVG} alt="Remaining players logo" />
 
-      <RemainingPlayersLogoContainer>
-        <RemainingPlayersLogo src={Logo} alt="Player logo vector" />
+        <span>بازیکن باقی مانده</span>
+      </RemainingPlayersRightContainer>
 
-        <RemainingPlayersLogoContainerText>
-          بازیکن باقی مانده
-        </RemainingPlayersLogoContainerText>
-      </RemainingPlayersLogoContainer>
+      <span>۱۵/۱۲</span>
     </RemainingPlayersContainer>
-  );
-};
+  )
+}
 
-export default RemainingPlayers;
+export default RemainingPlayers
