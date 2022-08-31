3d5585856f77147b15eee2102c5295d9ff631e73
Sohrab Chegini <sohrab.chegini@gmail.com>
2022-08-31 16:00:09 +0430

    Refactor budget component

---
+++ src/components/Budget/Budget.styled.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components"
+
+export const BudgetContainer = styled.div`
+  width: 17.125rem;
+  height: 7.625rem;
+  background: linear-gradient(266.07deg, #04f4f0 2.18%, #02fda2 125.43%);
+  box-shadow: 0rem 0rem 0.625rem rgba(0, 0, 0, 0.15);
+  border-radius: 1rem 1rem 0rem 0rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  & > span {
+    font-weight: 900;
+    font-size: 40px;
+    color: #3d195b;
+  }
+
+  & img {
+    width: 48px;
+    height: 48px;
+  }
+`
+
+export const BudgetRightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & span {
+    font-weight: 900;
+    font-size: 14px;
+    color: #3d195b;
+    margin-top: 0.3125rem;
+  }
+`
--- src/components/Budget/Budget.tsx
+++ src/components/Budget/Budget.tsx
@@ -1,68 +1,18 @@
-import styled from "styled-components";
-import Logo from "../../svg/empty-wallet.svg";
-
-const BudgetContainer = styled.div`
-  width: 274px;
-  height: 122px;
-  background: linear-gradient(266.07deg, #04f4f0 2.18%, #02fda2 125.43%);
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
-  border-radius: 16px 16px 0px 0px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const BudgetAmmountContainer = styled.div`
-  position: relative;
-  left: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const BudgetAmmount = styled.span`
-  font-family: "Vazirmatn";
-  font-style: normal;
-  font-weight: 900;
-  font-size: 40px;
-  color: #3d195b;
-`;
-
-const BudgetLogoContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  position: relative;
-  right: 18px;
-`;
-
-const BudgetLogoContainerText = styled.span`
-  font-family: "Vazirmatn";
-  font-style: normal;
-  font-weight: 900;
-  font-size: 14px;
-  color: #3d195b;
-`;
-
-const WalletLogo = styled.img`
-  width: 48px;
-  height: 48px;
-`;
+import { BudgetContainer, BudgetRightContainer } from "./Budget.styled"
+import WalletSVG from "../../svg/wallet.svg"
 
 const Budget = () => {
   return (
     <BudgetContainer>
-      <BudgetAmmountContainer>
-        <BudgetAmmount>۷۳</BudgetAmmount>
-      </BudgetAmmountContainer>
+      <BudgetRightContainer>
+        <img src={WalletSVG} alt="Wallet" />
 
-      <BudgetLogoContainer>
-        <WalletLogo src={Logo} />
+        <span>باقی مانده پول</span>
+      </BudgetRightContainer>
 
-        <BudgetLogoContainerText>باقی مانده پول</BudgetLogoContainerText>
-      </BudgetLogoContainer>
+      <span>۷۳</span>
     </BudgetContainer>
-  );
-};
+  )
+}
 
-export default Budget;
+export default Budget
