1274aff425babfb632dc96baa9f489f523d3a166
Saman-7 <Saman7Dev@gmail.com>
2022-09-02 15:11:09 +0430

    Refactor view switcher component

---
+++ src/components/ViewSwitcher/ViewSwicher.styled.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const ViewSwitcherContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  & .rahnema-logo {
+    background: white;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    position: relative;
+    bottom: -10px;
+  }
+
+  & .button-switch {
+    width: 272px;
+    height: 40px;
+    background: #f7f7f7;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 10;
+
+    & button {
+      height: 30px;
+      width: 131px;
+      border-radius: 4px;
+      font-weight: 700;
+      font-size: 14px;
+      line-height: 22px;
+      color: #3d195b;
+      border: none;
+      background-color: inherit;
+      &:first-child {
+        background-color: white;
+      }
+    }
+  }
+`;
--- src/components/ViewSwitcher/ViewSwitcher.tsx
+++ src/components/ViewSwitcher/ViewSwitcher.tsx
@@ -1,36 +1,11 @@
-import styled from "styled-components";
-import { ReactComponent as RahnemaLogo } from "../../svg/rahnema-logo.svg";
-
-const ViewSwitcherContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-
-  & .rahnema-logo {
-    background: white;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    border-radius: 8px;
-  }
-
-  & .change button {
-    height: 30px;
-    width: 131px;
-    border-radius: 4px;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 22px;
-    color: #3d195b;
-    background-color: white;
-    border: none;
-  }
-`;
+import { ReactComponent as RahnemaLogoSVG } from "../../svg/rahnema-logo.svg";
+import { ViewSwitcherContainer } from "./ViewSwicher.styled";
 
 const ViewSwitcher = () => {
   return (
     <ViewSwitcherContainer>
-      <RahnemaLogo className="rahnema-logo" />
-      <div className="change">
+      <RahnemaLogoSVG className="rahnema-logo" />
+      <div className="button-switch">
         <button>شماتیک ترکیب</button>
         <button>مشاهده لیست</button>
       </div>
