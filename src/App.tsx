47dbee2e8888c7de4cd988ad1ac08b234afe915a
Sohrab Chegini <sohrab.chegini@gmail.com>
2022-09-07 16:08:31 +0430

    Create initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import "./App.css";
-import MainPage from "./pages/MainPage";
+import "./App.css"
+import { Routes, Route } from "react-router-dom"
+import MainPage from "./pages/MainPage"
+import SigninPage from "./pages/SigninPage"
+import SignupPage from "./pages/SignupPage"
 
 const App = () => {
   return (
     <div className="app">
-      <MainPage />
+      <Routes>
+        <Route path="/" element={<SigninPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="main-page" element={<MainPage />} />
+      </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
