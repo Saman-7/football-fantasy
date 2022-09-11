0b67df9c5d88fda22a638d873f589c04635a6497
Sohrab Chegini <sohrab.chegini@gmail.com>
2022-09-11 12:12:56 +0430

    Add logic for remaining players and budget

--- src/components/RemainingPlayers/RemainingPlayers.tsx
+++ src/components/RemainingPlayers/RemainingPlayers.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react"
 import {
   RemainingPlayersContainer,
   RemainingPlayersRightContainer,
 } from "./RemainingPlayers.styled"
 import RemainingPlayersSVG from "../../svg/remaining-players.svg"
+import useMainPageStore from "../../store"
+import { numberEnglishToPersian } from "../../utils/numberEnglishToPersion"
 
 const RemainingPlayers = () => {
+  const [picked, setPicked] = useState(0)
+
+  const { picks } = useMainPageStore()
+  useEffect(() => {
+    picks.forEach(
+      (pick: {
+        is_captain: boolean
+        is_vice_captain: boolean
+        multiplier: number
+        player: {} | null
+        _id: string
+      }) => {
+        if (pick.player !== null) {
+          setPicked((prev) => prev + 1)
+        }
+      }
+    )
+  }, [picks])
+
   return (
     <RemainingPlayersContainer>
       <RemainingPlayersRightContainer>
@@ -13,7 +35,7 @@ const RemainingPlayers = () => {
         <span>بازیکن باقی مانده</span>
       </RemainingPlayersRightContainer>
 
-      <span>۱۵/۱۲</span>
+      <span>۱۵/{numberEnglishToPersian(picked.toString())}</span>
     </RemainingPlayersContainer>
   )
 }
