24ad28a47bd0067c28c012b7a3655896599f4cea
Sohrab Chegini <sohrab.chegini@gmail.com>
2022-09-11 09:45:22 +0430

    Add zustand and global state management

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import useMainPageStore from "../store"
 import styled from "styled-components"
 import Budget from "../components/Budget/Budget"
 import Header from "../components/Header/Header"
@@ -9,6 +11,7 @@ import Pitch from "../components/Pitch/Pitch"
 import RemainingPlayers from "../components/RemainingPlayers/RemainingPlayers"
 import SelectPlayer from "../components/SelectPlayer/SelectPlayer"
 import ViewSwitcher from "../components/ViewSwitcher/ViewSwitcher"
+import { useNavigate } from "react-router-dom"
 
 const MainPageContainer = styled.div`
   display: flex;
@@ -37,6 +40,30 @@ export enum PAGE_VIEW_SWITCHER {
 
 const MainPage = () => {
   const [page, setPage] = useState(PAGE_VIEW_SWITCHER.pitch)
+
+  const navigate = useNavigate()
+
+  const state = useMainPageStore()
+
+  const { setPicks, setBudget } = state
+
+  useEffect(() => {
+    const token = localStorage.getItem("token") || ""
+
+    if (token === "" || token === undefined) navigate("/signin")
+
+    axios
+      .get("http://178.216.248.37:8080/api/v1/managers/dashboard", {
+        headers: {
+          token: token,
+        },
+      })
+      .then((res) => res.data.data)
+      .then((data) => {
+        setPicks(data.teamId.picks)
+        setBudget(data.budget)
+      })
+  }, [])
 
   return (
     <MainPageContainer>
