ffce60ede0d6c4aee0edf2615a2eb9da38e79723
Saman-7 <Saman7Dev@gmail.com>
2022-09-16 21:29:56 +0430

    Create loading main component

---
+++ src/components/loading/LoadingMain.tsx
@@ -0,0 +1,17 @@
+import { Content, Dote, LoadingContainer } from "./LoadingMain.styled"
+
+const dotes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const Loading = () => {
+  return (
+    <LoadingContainer>
+      <Content>
+        {dotes.map((d) => (
+          <Dote dote={d}></Dote>
+        ))}
+      </Content>
+    </LoadingContainer>
+  )
+}
+
+export default Loading
