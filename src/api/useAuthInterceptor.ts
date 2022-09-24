15e4c34be56c1a358e81e90b2afc9fe72c376721
Saman-7 <Saman7Dev@gmail.com>
2022-09-24 11:24:08 +0330

    Create auth interceptor custom hook

---
+++ src/api/useAuthInterceptor.ts
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+export const useAuthInterseptor = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const reqId = axios.interceptors.request.use((request) => {
+      const localStorageToken = localStorage.getItem("token")
+      let token
+      try {
+        token = localStorageToken && JSON.parse(localStorageToken)
+      } catch {
+        // failed to parse token
+      }
+
+      request.headers = {
+        ...request.headers,
+        token,
+      }
+    })
+
+    const resId = axios.interceptors.response.use(undefined, (err) => {
+      if (err.status === 401) {
+        navigate("/signin")
+      }
+    })
+
+    return () => {
+      axios.interceptors.request.eject(reqId)
+      axios.interceptors.response.eject(resId)
+    }
+  })
+}
