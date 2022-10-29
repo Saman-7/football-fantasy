eb7d509aaa62c1f2aef0a8f23b098a168693b275
Saman-7 <Saman7Dev@gmail.com>
2022-10-29 20:18:54 +0330

    Add api to lates events component

--- src/components/MainPage/EventsPage/LatesEvents/LatesEvents.tsx
+++ src/components/MainPage/EventsPage/LatesEvents/LatesEvents.tsx
@@ -13,60 +13,75 @@ import {
   MatchWeek,
   PictureEvent,
 } from "./LatesEvents.styled"
-
-const enterData = ["Yasin", "Varane", "Fernandes"]
-const exitData = ["Haland", "Laporte", "Debruyne"]
+import { axios } from "../../../../api/axiosInstance"
+import { useEffect, useState } from "react"
+import { numberEnglishToPersian } from "../../../../utils/numberEnglishToPersion"
+import classNames from "classnames"
 
 const LatesEvents = () => {
+  const [events, setEvents] = useState<Array<any>>([])
+
+  useEffect(() => {
+    axios.get("/api/v1/feeds").then((res) => {
+      setEvents(res.data.data)
+    })
+  }, [])
+
   return (
     <LatesEventsContainer>
       <div className="title">
         <h1>آخرین رویدادها</h1>
       </div>
 
-      <BoxEventContent>
-        <PictureEvent>
-          <img src={profile1} alt="profile-1" />
-          <span>شایان رضایی</span>
-          <HeartLikeSVG className="heart-like-svg" />
-        </PictureEvent>
+      {events.map((user) => (
+        <BoxEventContent key={user.managerId}>
+          <PictureEvent>
+            <img src={profile1} alt="profile-1" />
+            <span>{`${user.first_name} ${user.last_name}`}</span>
+            <HeartLikeSVG
+              className={classNames("heart-like-svg", {
+                isLike: user.is_liked,
+              })}
+            />
+          </PictureEvent>
 
-        <MainBoxEvent>
-          <HeadBoxEvent>
-            <span>امتیاز هفته</span>
-            <div className="rating">
-              <StarSVG className="star-svg" />
-              <span>۱۰۴</span>
-            </div>
-          </HeadBoxEvent>
-
-          <ChangesPlayerContent>
-            <span>تعویض‌ها</span>
-
-            <div className="items-content">
-              <div className="item">
-                {enterData.map((name) => (
-                  <ItemEvent>
-                    <ArrowUpGreenSVG className="arrow-svg" />
-                    <span>{name}</span>
-                  </ItemEvent>
-                ))}
+          <MainBoxEvent>
+            <HeadBoxEvent>
+              <span>امتیاز هفته</span>
+              <div className="rating">
+                <StarSVG className="star-svg" />
+                <span>{numberEnglishToPersian(user.points.toString())}</span>
               </div>
-              <div className="item">
-                {exitData.map((name) => (
-                  <ItemEvent>
-                    <ArrowDownRedSVG className="arrow-svg" />
-                    <span>{name}</span>
-                  </ItemEvent>
-                ))}
+            </HeadBoxEvent>
+
+            <ChangesPlayerContent>
+              <span>تعویض‌ها</span>
+
+              <div className="items-content">
+                <div className="item">
+                  {user.substitutions.map((data: any, index: number) => (
+                    <ItemEvent key={index}>
+                      <ArrowUpGreenSVG className="arrow-svg" />
+                      <span>{data.in}</span>
+                    </ItemEvent>
+                  ))}
+                </div>
+                <div className="item">
+                  {user.substitutions.map((data: any, index: number) => (
+                    <ItemEvent key={index}>
+                      <ArrowDownRedSVG className="arrow-svg" />
+                      <span>{data.out}</span>
+                    </ItemEvent>
+                  ))}
+                </div>
               </div>
-            </div>
-          </ChangesPlayerContent>
-          <MatchWeek>
-            <span>#هفته‌ـ‌پنجم</span>
-          </MatchWeek>
-        </MainBoxEvent>
-      </BoxEventContent>
+            </ChangesPlayerContent>
+            <MatchWeek>
+              <span>#هفته‌ـ‌پنجم</span>
+            </MatchWeek>
+          </MainBoxEvent>
+        </BoxEventContent>
+      ))}
     </LatesEventsContainer>
   )
 }
